c0b8fb16fe96553330308499252aeceb44616e3a
NandhuVsona <naveenvsona@gmail.com>
2024-09-07 18:45:46 +0530

    first commit

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
+let staffsContainer = document.querySelector(".staffs-con-body");
+let hodsContainer = document.querySelector(".hods-con-body");
 
-let staffsContainer = document.querySelector(".staffs-container")
+let skeleton = `<div class="skeleton-card">
+                <div class="skeleton-img"></div>
+                <div class="staff-info">
+                  <h4 class="skeleton-name"></h4>
+                  <p class="skeletonposition"></p>
+                </div>
+              </div>`;
+for (let i = 0; i < 10; i++) {
+  hodsContainer.innerHTML += `<div class="hod-card">
+            <div class="skeleton-img-hod" style="border: 2px solid ${getRandomColor()}"></div>
+                <small></small>
+              </div>`;
+}
+for (let i = 0; i < 21; i++) {
+  staffsContainer.innerHTML += skeleton;
+}
+// let skeletonElements = document.querySelectorAll('[class^="skeleton"]');
+// skeletonElements.forEach((ele) => (ele.style.backgorundColor = "red"));
 loadData();
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 async function loadData() {
   let req = await fetch("https://staffsstatus.onrender.com/api/v1/staffs/");
   let res = await req.json();
-  let {data} = res;
-  data.forEach(staff =>{
+  if (res.status == "success") {
+    staffsContainer.innerHTML = " ";
+    hodsContainer.innerHTML = ' '
+  }
+  let { data } = res;
+  data.forEach((staff) => {
+    if (staff.position == "HOD") {
+      let template = `<div class="hod-card">
+                <img src="https://staffsstatus.onrender.com/uploads/${
+                  staff.image
+                }" style="border: 2px solid ${getRandomColor()}"  alt="">
+                <small style="background-color: inherit !important">ECE</small>
+              </div>`;
+      hodsContainer.innerHTML += template;
+    }
     let card = `<div class="card">
-            <img src="https://staffsstatus.onrender.com/uploads/${staff.image}" alt="">
+            <img src="https://staffsstatus.onrender.com/uploads/${staff.image}"alt="">
             <div class="staff-info">
-              <b class="name">${staff.name}</b>
-              <small class="position">${staff.position}</small>
+              <h4 style="background-color: inherit !important" class="name">${staff.name}</h4>
+              <p style="background-color: inherit !important" class="position">${staff.position}</p>
+              
             </div>
-            <button>View</button>
-          </div>`
-    staffsContainer.innerHTML += card
-  })
-  console.log(res);
+          </div>`;
+    staffsContainer.innerHTML += card;
+  });
 }
